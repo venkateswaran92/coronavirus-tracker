a6b8ce621827d1a23309ba37332346e80238f510
venkat <venkateswaran300@gmail.com>
2020-03-26 17:39:58 +0530

    webSocket chat application

---
+++ spring-boot-websocket/src/test/java/com/venkat/brains/SpringBootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkat.brains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
